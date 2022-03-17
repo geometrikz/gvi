d1aac45bd2ad2497ca7825e5ab07e5f2add0205c
Geoff Liu <geoffreyliu96@gmail.com>
2022-03-17 03:12:25 -0400

    update advi

--- src/advi.py
+++ src/advi.py
@@ -5,12 +5,17 @@ import numpy as np
 import jax
 from jax import random
 from functools import partial
+from jax.example_libraries import optimizers
 
+from sklearn import datasets
 
 key = random.PRNGKey(0)
 
 def generate_data(shape=(100,)):
     return random.normal(key, shape=shape) + 5
+
+def genereate_regression_data():
+    return
 
 @jit
 def log_priors(theta):
@@ -49,56 +54,104 @@ def log_var_sample(theta):
     eta_m = (random.normal(key=key, shape=(D, 50)) * sigma.reshape(-1,1)) + mu.reshape(-1, 1)
     return eta_m
 
+data = generate_data().reshape(-1,1)
+log_posterior = partial(log_posterior, y=data)
+
 def main():
-    data = generate_data().reshape(-1,1)
     theta = np.array([4.,0.5,0.01,0.01])
     lr = 1e-4
-    partial_log_posterior = partial(log_posterior, y=data)
-    # eta = log_var_sample(theta)
-    # partial_log_likelihood = partial(log_likelihood, y=data)
-    # print("LOGGG", eta[0].shape)
-    # print(partial_log_posterior(eta).shape, log_priors(eta).shape)
-    
-    # log_var = log_var_approx(theta, eta)
-    # log_post = partial_log_posterior(eta)
 
-    # print(log_post.shape, log_var.shape)
-    
-    # print(elbo(theta))
-
-    # def elbo(theta, y):
-    #     # eta = log_var_sample(theta)
-    #     D=2
-    #     mu, sigma = theta[:D,], theta[D:,]
-    #     eta = (random.normal(key=key, shape=(D, 50)) * sigma.reshape(-1,1)) + mu.reshape(-1, 1)
-
-    #     log_likel = jsp.stats.norm.logpdf(y, eta[0], eta[1]).sum(axis=0)
-    #     mu_prior = jsp.stats.norm.logpdf(eta[0], loc=0, scale=10)
-    #     sigma_prior = jsp.stats.norm.logpdf(eta[1], loc=1, scale=0.25)
-    #     log_post = log_likel + mu_prior + sigma_prior
-
-    #     log_var_approx2 = jsp.stats.norm.logpdf(eta[0], mu[0], sigma[0]) + jsp.stats.norm.logpdf(eta[1], mu[1], sigma[1])
-
-    #     # return jnp.mean(log_var_approx(theta, eta) - partial_log_posterior(eta))
-    #     return jnp.mean(log_var_approx2 - log_post)
-
+    @jit
     def elbo(theta):
         eta = log_var_sample(theta)
-        return jnp.mean(log_var_approx(theta, eta) - partial_log_posterior(eta))
-        # return jnp.mean(log_var_approx(theta, eta) - log_post)
+        return jnp.mean(log_var_approx(theta, eta) - log_posterior(eta))
 
-    # elbo = jit(value_and_grad(partial(elbo, y=data)))
     elbo = jit(value_and_grad(elbo))
 
-    for i in range(500):
+    opt_init, opt_update, get_params = optimizers.sgd(step_size=1e-3)
+    opt_state = opt_init(theta)
+    
+    for i in range(1000):
+        loss, der = elbo(theta)
+        # theta = theta - lr * der
 
-        # eta = log_var_sample(theta)
-        # elbo = lambda theta, eta: jnp.mean(log_var_approx(theta, eta) - partial_log_posterior(eta))
-        eval = elbo(theta)
-        theta = theta - lr * eval[1]
+        opt_state = opt_update(i, der, opt_state)
+        theta = get_params(opt_state)
+        # print(opt_state)
         if i % 10 == 0:
             print(theta)
     return
+
+def blr():
+    D = 5
+    x, y, coef = datasets.make_regression(n_samples=100, n_features=5, n_informative=5, bias=0, noise=5, coef=True)
+    y = y.reshape(-1,1)
+    theta = jnp.zeros(D*2)
+
+    @jit
+    def log_priors(theta):
+        # Independent priors
+        mu_prior = jsp.stats.norm.logpdf(theta, loc=0, scale=10).sum()
+        
+        return mu_prior
+
+    @jit
+    def log_likelihood(theta, y, x):
+        mu = jnp.dot(x, theta)
+        log_likel = jsp.stats.norm.logpdf(y, mu, 5).sum(axis=0)
+
+        return log_likel
+
+    @jit
+    def log_posterior(theta, y, x):
+        return (log_likelihood(theta, y, x) + log_priors(theta))
+
+    def log_var_approx(theta, samples):
+        D = len(theta)//2
+        mu, mu_var = theta[:D,], jnp.exp(theta[D:,])
+        # eta_m = (random.normal(key=key, shape=(2, 100)) * sigma) + mu.reshape(-1, 1)
+        # print(jnp.diag(sigma))
+        return jsp.stats.multivariate_normal.logpdf(samples.T, mu.reshape(D), jnp.diag(mu_var))
+        # return jsp.stats.norm.logpdf(x[0], mu[0], sigma[0]) + jsp.stats.norm.logpdf(x[1], mu[1], sigma[1])
+
+    def log_var_sample(theta):
+        D = len(theta)//2
+        mu, sigma = theta[:D,], jnp.exp(theta[D:,])
+        # print(sigma.shape, mu.shape)
+        eta_m = (random.normal(key=key, shape=(D, 50)) * sigma.reshape(-1,1)) + mu.reshape(-1, 1)
+        return eta_m
+
+    log_posterior = partial(log_posterior, y=y, x=x)
+
+    @jit
+    def elbo(theta):
+        eta = log_var_sample(theta)
+        return jnp.mean(log_var_approx(theta, eta) - log_posterior(eta))
+
+    elbo = jit(value_and_grad(elbo))
+
+    opt_init, opt_update, get_params = optimizers.sgd(step_size=1e-3)
+    opt_state = opt_init(theta)
+    
+    epochs = 100
+    logs = {'loss':jnp.zeros(epochs)}
+
+    for i in range(epochs):
+        loss, der = elbo(theta)
+
+        logs['loss'] = logs['loss'].at[i].set(loss)
+
+        # theta = theta - lr * der
+        opt_state = opt_update(i, der, opt_state)
+        theta = get_params(opt_state)
+        # print(opt_state)
+        if i % 10 == 0:
+            print(theta[:D], loss)
+    
+    print(coef)
+
+    
+
 
 
     
@@ -178,5 +231,7 @@ def run():
 
 if __name__ == '__main__':
     # print(generate_data())
-    log = main()
+    log = blr()
     # print(log['ELBO'][-1], log['theta'][-1])
+    # x, y, coef = datasets.make_regression(n_samples=100, n_features=10, n_informative=3, bias=5, coef=True)
+    # print(x.shape, y.shape, coef)
